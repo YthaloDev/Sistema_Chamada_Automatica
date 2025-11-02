4f2f350bde1325b1b042e76157378f026f0d3c13
Neyverson <neyversonjunior@gmail.com>
2025-11-02 18:16:16 -0300

    Página principal

--- chamada_automatica/urls.py
+++ chamada_automatica/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("pagina_web/", include("pagina_web.urls")),
+    path("", include("pagina_web.urls")),
     path("admin/", admin.site.urls),
 ]
