8e6af0a67017feef8cdac766b22675c3ffc26eca
Ythalo Ornelas <ythaloornelas@gmail.com>
2025-11-04 23:08:05 -0300

    implementação da parte do qr_code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Sistema_Chamada_Automatica
 Este é um projeto em Django que simula um sistema de chamadas, para facilitar a vida dos docentes, com uma chamada automática e eficiente;
+
--- pagina_web/views.py
+++ pagina_web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+import qrcode
 
 
 def index(request):
@@ -17,3 +18,24 @@ def chamada(request):
 
 def configuracao(request):
     return render(request, 'pagina_web/configuracao.html')
+
+    #imagem_qrcode = qrcode.make(link)
+    # Para mudar a extensão do arquivo penas mude o final .png para a extensão desejável
+    #
+    #gerar_qrcode("https://www.udf.edu.br")
+def gerar_qrcode(request):
+    if request.method == 'POST':
+        turma = request.POST.get('turma')  # Obtém a turma enviada no formulário
+        if turma:
+            imagem_qrcode = qrcode.make(f"link/{turma}")  # Gera o QR Code com a turma selecionada
+            response = HttpResponse(content_type="image/png")  # Define o tipo de conteúdo como imagem PNG
+
+            # Salva a imagem diretamente na resposta HTTP
+            imagem_qrcode.save(response, "PNG")
+
+            return response
+        else:
+            # Caso não tenha selecionado uma turma
+            return HttpResponse("Erro: Nenhuma turma selecionada", status=400)
+
+    return render(request, 'pagina_web/qrcode.html')
